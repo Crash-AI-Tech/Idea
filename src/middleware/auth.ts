3f0456ad13597d4b823989d31e607231b3498d65
nsaviour <wenjian.22@163.com>
2025-05-18 14:56:33 +0800

    删除原src

--- src/middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  const user = useSupabaseUser()
-
-  if (!user.value) {
-    // If user is not logged in, redirect to login page
-    // You might want to store the intended route (to.fullPath) to redirect back after login
-    return navigateTo('/login')
-  }
-})
